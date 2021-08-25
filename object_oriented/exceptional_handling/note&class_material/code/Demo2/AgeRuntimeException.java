dd301b0a9e75a2acf4b1c6331902c892ce4022d1
Yan <ywan328@outlook.com>
2021-08-25 19:35:36 +0800

    Add files via upload

---
+++ object_oriented/exceptional_handling/note&class_material/code/Demo2/AgeRuntimeException.java
@@ -0,0 +1,8 @@
+package com.java.demo2;
+
+public class AgeRuntimeException extends RuntimeException{
+
+    public AgeRuntimeException(String message) {
+        super(message);
+    }
+}
---
+++ object_oriented/exceptional_handling/note&class_material/code/Demo2/Demo.java
@@ -0,0 +1,8 @@
+package com.java.demo2;
+
+public class Demo {
+    public static void main(String[] args) {
+        Person p = new Person();
+        p.setAge(-1);
+    }
+}
